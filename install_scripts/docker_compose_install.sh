8048fcd9251486946b6d9c4925102f5678ce638c
Hatem Jaber <hatem@7079.net>
2021-03-02 14:49:23 -0500

    added install scripts

---
+++ install_scripts/docker_compose_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
