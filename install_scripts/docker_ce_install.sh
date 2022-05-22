3e6f8fbb79f97f2f9ec316112099ffa4b274aabb
Hatem Jaber <hatem@7079.net>
2022-05-22 11:48:13 -0400

    updated docker install scripts

--- install_scripts/docker_ce_install.sh
+++ install_scripts/docker_ce_install.sh
@@ -2,9 +2,10 @@
 
 apt update
 apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
+apt-cache policy docker-ce
 apt install docker-ce -y
 systemctl status docker
 usermod -aG docker ${USER}
