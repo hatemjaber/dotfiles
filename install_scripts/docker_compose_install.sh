82ce664e14c73fe4a7e5a69f8b49b36add74b8d1
Hatem Jaber <hatem@7079.net>
2022-06-14 20:06:54 -0400

    updated docker-compose script

--- install_scripts/docker_compose_install.sh
+++ install_scripts/docker_compose_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
