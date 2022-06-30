a24e92b78e71552403866ce79873c6b84f981f91
Hatem Jaber <hatem@7079.net>
2022-06-30 11:27:39 -0400

    updated docker install

--- install_scripts/docker_ce_install.sh
+++ install_scripts/docker_ce_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 apt update
-apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt upgrade -y
+apt remove docker docker-engine docker.io containerd runc
+apt install ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt-cache policy docker-ce
-apt install docker-ce -y
-systemctl status docker
-usermod -aG docker ${USER}
-
+apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
